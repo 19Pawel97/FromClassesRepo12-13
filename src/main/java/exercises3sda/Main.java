870eea415cbb6784c2a9e71005360a22ac10e67f
19Pawel97 <19pawel9772@gmail.com>
2020-12-12 09:58:13 +0100

    Exercises 1 and 2 from 3sda.

--- src/main/java/exercises3sda/Main.java
+++ src/main/java/exercises3sda/Main.java
@@ -1,7 +1,19 @@
 package exercises3sda;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!!!");
+        Cat cat1 = new Cat("Kazimierz");
+        Cat cat2 = new Cat("Mruczek");
+        Cat cat3 = new Cat("Garfield");
+        List<Cat> cats = new ArrayList<>();
+        cats.add(cat1);
+        cats.add(cat2);
+        cats.add(cat3);
+        for (Cat cat: cats) {
+            cat.makeSound();
+        }
     }
 }
