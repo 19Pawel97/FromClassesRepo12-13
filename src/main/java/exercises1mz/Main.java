7ec8d6196445070c295d84363a32ee9db8679bae
19Pawel97 <19pawel9772@gmail.com>
2020-12-13 11:12:45 +0100

    From classes exercise 1, 1mn main added

---
+++ src/main/java/exercises1mz/Main.java
@@ -0,0 +1,12 @@
+package exercises1mz;
+
+public class Main {
+    public static void main(String[] args) {
+        Exercise1 myArray = new Exercise1(25);
+        myArray.addToArray();
+        myArray.printArray();
+        myArray.printArraySize();
+        myArray.multiplication(10);
+        myArray.printArray();
+    }
+}
